80859c375aa2520735a3a8f1a718df5fc65a444b
Alejandrolopez19 <alejandro190920@gmail.com>
2023-02-16 23:04:09 -0500

    Commit 1
    Version Sin Vista

---
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+ // crear base de datos nombre springboot
+    // para añadir usuarios en metodo post Body  formato json datos de bd   { nombre:"",email:"","prioridad":2}
+    // busqueda por id http://localhost:8080/usuario/2
+    //La busqueda por prioridad http://localhost:8080/usuario/query?prioridad=3
+    //(Se debe realizar delate por POSTMAN)   MODE DELATE:  localhost:8080/usuario/2
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping( path = "/{id}")
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping( path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if (ok){
+            return "Se eliminó el usuario con id " + id;
+        }else{
+            return "No pudo eliminar el usuario con id" + id;
+        }
+    }
+
+}
